5393874eed28217a17f3c9643336955b081d4b92
Amritsks <amritsks1111@gmail.com>
2026-03-10 12:18:31 +0530

    feat: navbar link

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,14 +7,14 @@ import Logo from "../assets/athery_Logo.svg";
 const NavBar = async () => {
   const links = [
     { name: "Home", href: "/" },
-    { name: "Our Story", href: "#" },
+    { name: "Our Story", href: "/our-story" },
     { name: "Product", href: "/product" },
     { name: "Contact", href: "#" },
   ];
   return (
     <nav className="bg-nav-background text-nav-foreground fixed top-8 right-0 left-0 z-20 mx-auto flex h-16 w-full max-w-2xl items-center justify-between rounded-3xl px-4 backdrop-blur-2xl sm:max-w-4xl md:max-w-6xl">
       <div className="text-medium relative flex w-full items-center justify-between px-2">
-        <span className="flex shrink-0 items-center justify-start">
+        <Link className="flex shrink-0 items-center justify-start" href={"/"}>
           <Image
             src={Logo}
             alt="athery logo"
@@ -22,7 +22,7 @@ const NavBar = async () => {
             height={100}
             className="ml-4"
           />
-        </span>
+        </Link>
 
         <span className="absolute left-1/2 flex -translate-x-1/2 items-center justify-between gap-2">
           {links.map((link) => (
