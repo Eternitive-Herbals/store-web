9ff20a8399a3e7f1b1a876d74b9cebb7bb245cf3
Vanshika <vanshikavalecha.20@gmail.com>
2026-06-10 17:56:44 +0530

    fix: update hero background image and height, and remove reviews section

--- app/(main)/our-story/_components/HeroSection.tsx
+++ app/(main)/our-story/_components/HeroSection.tsx
@@ -1,15 +1,14 @@
 import Image from "next/image";
-import BackgroundTexture from "@/assets/background-texture-brown-1.svg";
-
+import StoryBg from "@/assets/our-story-bg.png";
 export default function HeroSection() {
   return (
-    <section className="relative flex h-[320px] items-center justify-center overflow-hidden bg-primary-background">
+    <section className="relative flex h-[567px] items-center justify-center overflow-hidden bg-primary-background">
       <Image
-        src={BackgroundTexture}
+        src={StoryBg}
         alt="Background Texture"
         fill
         sizes="100vw"
-        className="object-cover opacity-20"
+        className="object-cover opacity-50"
       />
       <div className="absolute inset-0 bg-gradient-to-b from-black/50 to-black/30" />
       <h1 className="font-comfortaa relative z-10 text-5xl font-bold tracking-tight text-white">
