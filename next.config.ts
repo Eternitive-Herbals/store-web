829ac05c79d314e5e66c993bcbe7065f2bf3a99f
Sahil_Kumar <sahil.kumar.23cse@bmu.edu.in>
2026-07-06 13:13:03 +0530

    chore: remove eslint config option not supported in nextjs 16

--- next.config.ts
+++ next.config.ts
@@ -18,11 +18,6 @@ const nextConfig: NextConfig = {
     ],
     unoptimized: true,
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // the project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
