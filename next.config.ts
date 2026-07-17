f48a84661552f0c7a5d5b488edca962741c618dd
Sahil_Kumar <sahil.kumar.23cse@bmu.edu.in>
2026-07-17 18:24:59 +0530

    next config updated

--- next.config.ts
+++ next.config.ts
@@ -11,12 +11,12 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "aethery-s3-bucket-295753750949-us-east-2-an.s3.us-east-2.amazonaws.com",
       }
-      // {
-      //   protocol: "https",
-      //   hostname: "**", // Fallback for any other domains, though specific is better
-      // }
+      {
+        protocol: "https",
+        hostname: "**", 
+       }
     ],
-    // unoptimized: true,
+    
   },
 };
 
