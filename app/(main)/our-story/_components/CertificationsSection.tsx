a10fad46d336019d645c1f70bc628ac514bae1dc
Sahil_Kumar <sahil.kumar.23cse@bmu.edu.in>
2026-07-28 10:23:57 +0530

    fix: Clean up code formatting and resolve merge conflicts in CertificationsSection

--- app/(main)/our-story/_components/CertificationsSection.tsx
+++ app/(main)/our-story/_components/CertificationsSection.tsx
@@ -1,4 +1,4 @@
-<<<<<<< Updated upstream
+
 import GMP from "@/assets/gmp.svg";
 import WHO from "@/assets/WHO.svg";
 import SAT from "@/assets/satisfaction.svg";
@@ -27,38 +27,3 @@ export default function CertificationsSection() {
     </section>
   );
 }
-=======
-
-import GMP from "@/assets/gmp.svg"
-import WHO from "@/assets/WHO.svg"
-import SAT from "@/assets/satisfaction.svg"
-import ISO from "@/assets/ISO.svg"
-import HACCP from "@/assets/haccp.svg"
-import Image from "next/image"
-
-const certs = [
-  WHO,HACCP,ISO,SAT,GMP
-];
-
-export default function CertificationsSection() {
-  return (
-    <section className="mx-[calc(100dvw/24)] mb-12 flex flex-col items-center gap-8 rounded-3xl bg-primary-background px-12 py-10">
-      <h2 className="font-comfortaa text-xl font-bold text-white">
-        Our Certification
-      </h2>
-      <div className="flex flex-wrap items-center justify-center gap-7">
-        {certs.map((cert, idx) => (
-          <Image
-          alt={`${cert} certificate`}
-          src={cert}
-          key={idx} 
-          size={100}
-          priority
-          />
-          
-        ))}
-      </div>
-    </section>
-  );
-}
->>>>>>> Stashed changes
